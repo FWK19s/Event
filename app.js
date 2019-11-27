1081f0c623efb6ae8776e4e9a3e44af1c9169b46
Taleb_84 <taleb_n1@hotmail.com>
2019-11-27 17:33:57 +0100

    modify app.js
    add file.txt and app.js to show how node application works

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
-const events = require('events');
-const eventEmitter = new events.EventEmitter();
-const connectHandler = function connected() {
-   console.log('connection succesful.'); 
-   eventEmitter.emit('data_received');
-}
-eventEmitter.on('connection', connectHandler);
-eventEmitter.on('data_received', function() {
-   console.log('data received succesfully.');
+// const events = require('events');
+// const eventEmitter = new events.EventEmitter();
+// const connectHandler = function connected() {
+//    console.log('connection succesful.'); 
+//    eventEmitter.emit('data_received');
+// }
+// eventEmitter.on('connection', connectHandler);
+// eventEmitter.on('data_received', function() {
+//    console.log('data received succesfully.');
+// });
+// eventEmitter.emit('connection');
+// console.log("Program Ended.");
+
+const fs = require('fs');
+fs.readFile('file.txt', function (err, data){
+if (err) {
+console.log(err);
+return
+} console.log(data.toString());
 });
-eventEmitter.emit('connection');
-console.log("Program Ended.");
+console.log("End of the program");
